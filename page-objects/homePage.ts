1cc8e9dca8fd068eda594fde47c94c9fbabd4f40
EJ Reilly <edwardreillycomputers@gmail.com>
2019-08-10 16:28:40 -0400

    Added function for the dress tab

--- page-objects/homePage.ts
+++ page-objects/homePage.ts
@@ -1,9 +1,14 @@
 // import {browser, element, by, By, $, $$, ExpectedConditions}
 import { browser, element, by } from 'protractor';
 
-export class AutomationPracticeHomepage {
+export class HomePage {
+    dressesTab = element.all(by.css("a[title=\"Dresses\"]")).last();
 
     async get(): Promise<any> {
         return await browser.get('http://automationpractice.com/');
+    }
+
+    async clickDressesTab(): Promise<any> {
+        return await this.dressesTab.click();
     }
 }
