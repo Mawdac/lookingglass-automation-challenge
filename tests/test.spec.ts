544431c36fe57587b36e12aa6bb8fc2dda60c4e0
EJ Reilly <edwardreillycomputers@gmail.com>
2019-08-10 16:29:33 -0400

    Added steps for the dress page

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,13 +1,35 @@
-import { AutomationPracticeHomepage } from '../page-objects/homePage';
+import { HomePage } from '../page-objects/homePage';
+import { DressPage } from '../page-objects/dressPage';
 import { browser } from 'protractor';
 
 describe('automation practice homepage', function () {
+    let homePage = new HomePage();
+    let dressPage = new DressPage();
+
     beforeEach(async function (): Promise<any> {
         await browser.waitForAngularEnabled(false); // non-angular site
+        await browser.driver.manage().window().maximize();
     });
 
     it('should navigate to the homepage', async function (): Promise<any> {
-        let automationPracticeHomepage = new AutomationPracticeHomepage();
-        await automationPracticeHomepage.get();
+        await homePage.get();
+
+        await homePage.clickDressesTab();
+
+        expect(browser.getCurrentUrl()).toEqual("http://automationpractice.com/index.php?id_category=8&controller=category");
+
+        await browser.sleep(2000);
+
+        await dressPage.clickListView();
+
+        await browser.sleep(2000);
+
+        await dressPage.clickDressAddToCartButton(3);
+
+        await browser.sleep(2000);
+
+        await dressPage.clickCloseCartPopup();
+
+        await browser.sleep(2000);
     });
 });
