4d650beef58e0070d4899a9c76e3579239fbdc62
EJ <edwardreillycomputers@gmail.com>
2019-08-10 22:04:47 -0400

    Increased sleep

--- page-objects/dressPage.ts
+++ page-objects/dressPage.ts
@@ -79,7 +79,7 @@ export class DressPage {
      * @memberof DressPage
      */
     async clickContinueCartPopup(): Promise<any> {
-        await browser.sleep(500); // minor sleep to prevent timing issues with popup displaying the element
+        await browser.sleep(800); // minor sleep to prevent timing issues with popup displaying the element
         return await this.continueCartPopup.click();
     }
 
