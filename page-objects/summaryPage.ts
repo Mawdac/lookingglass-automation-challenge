e663036953d1aec0c9138352f62cbe849bed85e4
EJ Reilly <edwardreillycomputers@gmail.com>
2019-08-12 00:53:38 -0400

    Tried different ways to grab the cart elements

--- page-objects/summaryPage.ts
+++ page-objects/summaryPage.ts
@@ -10,32 +10,33 @@ export class SummaryPage {
         return await expect(product.isDisplayed()).toBeTruthy();
     }
 
-    async checkProductPriceById(productId: any): Promise<boolean> {
+    async verifyProductPriceById(productId: any): Promise<any> {
         let price, discount, discountPrice, calculatedPrice;
         var product = await this.getProductById(productId);
-        await element(product).element(by.css(".cart_unit")).element(by.css(".special-price")).isDisplayed().then( async function (discountedProduct) {
-            if (discountedProduct) {
-                price = element(product).element(by.css(".cart_unit")).element(by.css(".old-price")).getText();
-                discount = element(product).element(by.css(".cart_unit")).element(by.css(".price-percent-reduction")).getText();
-                discountPrice = element(product).element(by.css(".cart_unit")).element(by.css(".price")).getText();
-                
-                price = Number(price.replace("$", ""));
-                discount = discount.replace("%", "");
-                discount = Number(discount.replace("-", "")) * (1/100);
-                discountPrice = Number(discount.replace("$", ""));
+        // discountPrice = await element(product).element(by.css(".special-price"));
+        // await element(product).element(by.css(".special-price")).isDisplayed().then(async function (discountedProduct) {
+        //     if (discountedProduct) {
+        price = element(product).element(by.css(".old-price")).getText();
+        discount = element(product).element(by.css(".price-percent-reduction")).getText();
+        discountPrice = element(product).element(by.css(".price")).getText();
 
-                calculatedPrice = price - (price * discount);
+        price = Number(price.replace("$", ""));
+        discount = discount.replace("%", "");
+        discount = Number(discount.replace("-", "")) * (1 / 100);
+        discountPrice = Number(discount.replace("$", ""));
 
-                console.log(price);
-                console.log(discount);
-                console.log(discountPrice);
-                console.log(calculatedPrice);
+        calculatedPrice = price - (price * discount);
 
-                return await expect(calculatedPrice).toEqual(discountPrice);
-            } else {
-                return await expect(element(product).element(by.css(".cart_unit")).element(by.css("price")).isDisplayed).toBeTruthy(); // TODO: this really needs refactoring
-            }
-        });
-        return;
+        // console.log(price);
+        // console.log(discount);
+        // console.log(discountPrice);
+        // console.log(calculatedPrice);
+
+        return await expect(calculatedPrice).toEqual(discountPrice);
+        // } else {
+        //     return await expect(element(product).element(by.css(".cart_unit")).element(by.css("price")).isDisplayed).toBeTruthy();
+        // }
+        // });
+        // return;
     }
 }
